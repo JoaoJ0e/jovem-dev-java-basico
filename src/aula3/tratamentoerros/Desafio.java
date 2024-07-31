1164bd96392355d29411a2d4833f4338389d3632
João Griebner <joao.griebner@gmail.com>
2024-07-31 15:13:32 -0300

    Upd desafio aula3

--- src/aula3/tratamentoerros/Desafio.java
+++ src/aula3/tratamentoerros/Desafio.java
@@ -12,42 +12,72 @@ public class Desafio {
 	public static void main(String[] args) {
 		
 		Venda venda = new Venda();
-		venda.setNomeCliente(JOptionPane.showInputDialog("Nome do cliente:"));
-		int qtdItens = Integer.parseInt(JOptionPane.showInputDialog("Insira a quantidade de itens:"));
-
-		try {
-			if (qtdItens <= 0 || qtdItens > 10) {
-				throw new VendaException("Quantidade inválida: Insira uma quantidade de 1 à 10.");
-			}
-			String nome = "";
-			double preco = 0;
-			
-			ArrayList<Item> listaItens = new ArrayList<>();
-			
-			for (int i = 0; i < qtdItens; i++) {
-				nome = JOptionPane.showInputDialog("Insira o nome do " + (i+1) + "º item: ");
-				do { 
-					//Validação do preço do item
-					try {
-						preco = Double.parseDouble(JOptionPane.showInputDialog("Insira o preço do " + (i+1) + "º item: "));
-						if (preco <= 0) {
-							throw new PrecoIncorretoException("Valor não pode ser menor ou igual a zero.");
-						}
-					} catch (PrecoIncorretoException e) {
-						JOptionPane.showMessageDialog(null, e.getMessage());
-					}
-				} while (preco <= 0);
-				
-				Item item = new Item(nome, preco);
-				listaItens.add(item);
-				
-			}
-			venda.setItens(listaItens);
-			JOptionPane.showMessageDialog(null, venda.geraCupomFiscal());
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Erro: "+ e.getMessage());;
-		}
 		
+		try {
+			venda.setNomeCliente(JOptionPane.showInputDialog("Nome do cliente:"));
+			ArrayList<Item> lista = pedeItens();
+			venda.setItens(lista);
+			JOptionPane.showMessageDialog(null, venda.geraCupomFiscal());
+		} catch (VendaException e) {
+			JOptionPane.showMessageDialog(null, "Erro: "+ e.getMessage());
+		}
 	}
 		
+
+	public static ArrayList<Item> pedeItens() throws VendaException {
+		int qtdItens = validaQtdItens();
+		String nome = "";
+		double preco = 0;
+			
+		ArrayList<Item> listaItens = new ArrayList<>();
+		
+		for (int i = 0; i < qtdItens; i++) {
+			nome = JOptionPane.showInputDialog("Insira o nome do " + (i+1) + "º item: ");
+			preco = validaPreco();			
+			Item item = new Item(nome, preco);
+			listaItens.add(item);
+		}
+		
+		return listaItens;
+		
+	}
+	
+	
+	public static int validaQtdItens() throws VendaException {
+		boolean continua = true;
+		do {
+			try {
+				int qtdItens = Integer.parseInt(JOptionPane.showInputDialog("Insira a quantidade de itens:"));
+				if (qtdItens <= 0 || qtdItens > 10) {
+					throw new VendaException("Quantidade inválida: Insira uma quantidade de 1 à 10.");
+				}
+				continua = false;
+				return qtdItens;
+			} catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "Erro: Valor inválido!");
+			}
+		} while (continua);
+		return 0;
+	}
+	
+	public static double validaPreco() {
+		boolean continua = true;
+		double preco = 0;		
+		do { 
+			try {
+				preco = Double.parseDouble(JOptionPane.showInputDialog("Insira o preço do  item: "));
+				if (preco <= 0) {
+					throw new PrecoIncorretoException("Valor não pode ser menor ou igual a zero.");
+				}
+				continua = false;
+				return preco;
+			} catch (PrecoIncorretoException | NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
+			} 
+		} while (continua);
+		return preco;
+	}
 }
+
+	
+
